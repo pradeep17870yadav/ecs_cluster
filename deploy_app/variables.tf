490a7118fda4c9b7ba141f454d31a44d317a3c2b
pradeep17870yadav <pradeepyadav1788000@gmail.com>
2024-08-11 23:42:58 +0530

    variables modified

--- deploy_app/variables.tf
+++ deploy_app/variables.tf
@@ -5,3 +5,9 @@ variable "Region" {
 variable "cluster_name" {
   
 }
+
+variable "POSTGRES_PASSWORD" {}
+
+variable "POSTGRES_USER" {}
+
+variable "POSTGRES_DB" {}
--- variables.tf
+++ variables.tf
@@ -3,8 +3,3 @@ variable "cluster_name" {
 
 variable "Region" {}
 
-variable "POSTGRES_PASSWORD" {}
-
-variable "POSTGRES_USER" {}
-
-variable "POSTGRES_DB" {}
